d993007b32bd90f2a570d5889078ebcd4fda9890
gibranamparan <gibranamparand@hotmail.com>
2016-01-05 10:17:30 -0700

    este es el primer commit

--- assets/js/myJS.js
+++ assets/js/myJS.js
@@ -1,11 +1,11 @@
 angular.module('formApp',[]).controller('formController',['$scope','$http',
-	function($scope,$http){
-	
+function($scope,$http){
 	$scope.compra={
 		cantidad:10,
 		costo:1
 	};
 
+	getProductos();
 	//Se toman los datos del server a través de 
 	//comunicacion por sockets.
 	function getProductos(){
@@ -14,8 +14,6 @@ angular.module('formApp',[]).controller('formController',['$scope','$http',
 			$scope.$apply();
 		});
 	}
-
-	getProductos();
 
 	/*Se detecta algun cambio en la BD sobre cierta entidad 
 	especificada y se ejecuta un evento*/
@@ -28,14 +26,4 @@ angular.module('formApp',[]).controller('formController',['$scope','$http',
 			getProductos();
 		}
 	});
-
-	/*$http.get('/productos').then(function(response){
-		$scope.productos = response.data;
-	});*/
-
-	/*$scope.productos=[
-		{id:1, nombre:"1kg de Tomate",costo:10},
-		{id:2, nombre:"1kg de Lechuga",costo:20},
-		{id:3, nombre:"Jabon Palmolive",costo:30},
-		{id:4, nombre:"Shampoo",costo:40}];*/
 }]);
