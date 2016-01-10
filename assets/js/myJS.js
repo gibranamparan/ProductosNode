9d7e71d4bc6ba2a844de069c9707f4258de93d03
Gibran Amparan <GibranAmparanD@hotmail.com>
2016-01-09 19:36:18 -0700

    Prueba con instancia de app en angular

--- assets/js/myJS.js
+++ assets/js/myJS.js
@@ -2,17 +2,22 @@ var app = angular.module('formApp',[]);
 
 app.controller('formController', ['$scope',function($scope){
 
-	getProductos();
+	//getProductos();
 
 	//Se toman los datos del server a través de 
 	//comunicacion por sockets.
-	function getProductos(){
+	/*function getProductos(){
 		io.socket.get('/productos', function(data){
 			//$scope.productos = data;
 			//$scope.$apply();
 			$scope.productos = 'Unos productos';
 			$scope.$apply();
 		});
-	}
+	}*/
+	$scope.productos = 'Unos productos';
 	
 }]);
+
+/*app.controller('mainController', ['$scope', '$http', function($scope) {
+    $scope.message = 'HOORAY!'; 
+}]); */
